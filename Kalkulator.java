004ae2b65705d213dff8d060459fd885bf0edca3
FooDawn <mal.medo987@gmail.com>
2021-09-26 23:36:56 +0200

    Update

--- Kalkulator.java
+++ Kalkulator.java
@@ -7,23 +7,14 @@ public class Kalkulator {
 
 		// max 
 		// min
-
-		// Vsota: %d + %d = %d\n"
-		// Odstevanje: %d - %d = %d
-		// Mnozenje: %d * %d = %d\n
-		// Deljenje: %d / %d = %d\n
-		// Ostanek: %d / %d => ostanek %d\n
-		// Potenciranje: %d ^ %d = %f\n
-		// Potenciranje: %d ^ %d = %d\n
-
-		// Korenjanje sqrt: koren %d 
-		// Korenjanje pow: koren %d = %f\n", a, Math.pow(a, 0.5));
+	
 
 		// postevanko napisite - v obliki %d * %d = %d
 
 		/* podaj dolzino in zapisi toliko stevil
 			in zapisi povprecje
 		*/
+		
 	}
 
 }
---
+++ UgibanjeStevil.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class UgibanjeStevil {
+	
+	public static void main(String[] args) {
+		// uporabnik vnese stevilo in mu program pove ali je to stevilo previsoko ali prenizko
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		
+
+		
+	}
+}
