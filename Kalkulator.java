b4c6ffae71cffe4887cb780a234a4971140d03f1
FooDawn <mal.medo987@gmail.com>
2021-09-27 15:16:10 +0200

    druge dve nalogi

--- Kalkulator.java
+++ Kalkulator.java
@@ -7,14 +7,28 @@ public class Kalkulator {
 
 		// max 
 		// min
-	
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
 
 		// postevanko napisite - v obliki %d * %d = %d
+		postevanka(a, b);
+		System.out.println();
+		postevanka(b, a);
+		System.out.println();
+		postevanka(10, 5);
 
 		/* podaj dolzino in zapisi toliko stevil
 			in zapisi povprecje
 		*/
-		
+	}
+
+	public static void postevanka(int x, int y){
+		for (int i = 1; i <= y ; i++ ) {
+			System.out.printf("%d * %d = %d\n", x, i, x * i);
+			
+		}
 	}
 
 }
--- UgibanjeStevil.java
+++ UgibanjeStevil.java
@@ -8,8 +8,27 @@ public class UgibanjeStevil {
 		// uporabnik vnese stevilo in mu program pove ali je to stevilo previsoko ali prenizko
 		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
-		
 
+		int iskanoStevilo = rd.nextInt(50) + 1;
+		int vnos;
+		boolean nismoNasli = true;
+
+		while (nismoNasli){
+			System.out.println("vpisi cifro:");
+			vnos = sc.nextInt();
+			if (vnos == iskanoStevilo){
+				System.out.println("Cestitke");
+				nismoNasli = false;
+			} else if (vnos < iskanoStevilo) {
+				System.out.println("Prenizko stevilo ste izbrali");
+			}else {
+				System.out.println("Previsoko stevilo ste izbrali");
+
+			}
+
+		}
+
+		// System.out.println(iskanoStevilo);
 		
 	}
 }
